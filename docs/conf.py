368c302f8bdd2c75d8851bcfd3fff1deb2f7c66f
Peter Williams <peter@newton.cx>
2023-03-29 12:01:21 -0400

    docs/conf.py: reformat with "black"

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
 
-project = 'wwt_api_client'
-author = 'Peter K. G. Williams'
-copyright = '2019 ' + author
+project = "wwt_api_client"
+author = "Peter K. G. Williams"
+copyright = "2019 " + author
 
-release = '0.1.0dev0'
+release = "0.1.0dev0"
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx_automodapi.automodapi',
-    'sphinx_automodapi.smart_resolver',
-    'numpydoc',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx_automodapi.automodapi",
+    "sphinx_automodapi.smart_resolver",
+    "numpydoc",
 ]
 
-master_doc = 'index'
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+master_doc = "index"
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 numpydoc_class_members_toctree = False
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
-html_logo = 'images/logo.png'
+html_theme = "alabaster"
+html_static_path = ["_static"]
+html_logo = "images/logo.png"
