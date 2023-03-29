8634624454b14e9a8d2dc8b612b7504912d133e6
Peter Williams <peter@newton.cx>
2023-03-29 11:57:58 -0400

    setup.py: reformat with "black"

--- setup.py
+++ setup.py
@@ -10,60 +10,59 @@ from setuptools import setup
 
 from setupbase import find_packages, get_version
 
-name = 'wwt_api_client'
-version = get_version(pjoin(name, '_version.py'))
+name = "wwt_api_client"
+version = get_version(pjoin(name, "_version.py"))
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     LONG_DESCRIPTION = f.read()
 
 setup_args = dict(
-    name            = name,
-    description     = 'An API client for the AAS WorldWide Telescope web services',
-    long_description = LONG_DESCRIPTION,
-    version         = version,
-    packages        = find_packages(),
-    author          = 'Peter K. G. Williams',
-    author_email    = 'peter@newton.cx',
-    url             = 'https://github.com/WorldWideTelescope/wwt_api_client',
-    license         = 'BSD',
-    platforms       = "Linux, Mac OS X, Windows",
-    keywords        = ['Science'],
-    classifiers     = [
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+    name=name,
+    description="An API client for the AAS WorldWide Telescope web services",
+    long_description=LONG_DESCRIPTION,
+    version=version,
+    packages=find_packages(),
+    author="Peter K. G. Williams",
+    author_email="peter@newton.cx",
+    url="https://github.com/WorldWideTelescope/wwt_api_client",
+    license="BSD",
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Science"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Multimedia :: Graphics",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    include_package_data = True,
-    install_requires = [
-        'requests',
-        'six',
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "six",
     ],
-    extras_require = {
-        'test': [
-            'httpretty',
-            'pytest',
-            'pytest-cov',
-            'pytest-mock',
+    extras_require={
+        "test": [
+            "httpretty",
+            "pytest",
+            "pytest-cov",
+            "pytest-mock",
         ],
-        'docs': [
-            'sphinx>=1.6',
-            'sphinx-automodapi',
-            'numpydoc',
-            'sphinx_rtd_theme',
+        "docs": [
+            "sphinx>=1.6",
+            "sphinx-automodapi",
+            "numpydoc",
+            "sphinx_rtd_theme",
         ],
     },
-    entry_points = {
-    },
+    entry_points={},
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
